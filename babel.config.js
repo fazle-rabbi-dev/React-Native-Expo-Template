92294ca99318906f39d3922e48b6f9e86e664b6f
fh-rabbi <frabby983@gmail.com>
2024-04-30 11:58:00 +0600

    Added Absolute Path Resolver Feature

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          alias: {
+            src: "./src",
+            "@assets": "./assets",
+            "@components": "./components",
+            "@constants": "./constants",
+            "@hooks": "./hooks",
+            "@utils": "./lib/utils.js"
+          }
+        }
+      ]
+    ]
   };
 };
